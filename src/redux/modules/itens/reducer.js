7e89fd9005d7f86a70694891ee9ccc5963ffbd8c
Cassiano Bitencourt <tricecautopo@gmail.com>
2020-12-14 11:07:36 -0300

    Adding localStorage

--- src/redux/modules/itens/reducer.js
+++ src/redux/modules/itens/reducer.js
@@ -1,29 +1,40 @@
 import { CHECKTODO, MODIFYTODO, DELETETODO, CREATENEWTODO } from "./types"
 
-const defaultState = []
+const defaultState = JSON.parse(localStorage.getItem("TODOs")) || []
+
+const writeInLocalStorage = (newState) =>
+    localStorage.setItem('TODOs', JSON.stringify(newState))
 
 const reducer = (state = defaultState, action) => {
     switch (action.type) {
         case CREATENEWTODO:
-            return [...state, { text: "", checked: false, id: state.length }]
+            let newState = [...state, { text: "", checked: false, id: state.length }]
+
+            writeInLocalStorage(newState)
+            return newState
         case CHECKTODO:
             const { todoId, checked } = action.payload
 
-            let newState = [...state]
-            newState.find((todo) => todo.id === todoId).checked = checked
+            let newState2 = [...state]
+            newState2.find((todo) => todo.id === todoId).checked = checked
 
-            return newState
+            writeInLocalStorage(newState2)
+            return newState2
         case MODIFYTODO:
             const { todoId: todoIndex, text } = action.payload
 
             let newStateModify = [...state]
             newStateModify.find((todo) => todo.id === todoIndex).text = text
 
+            writeInLocalStorage(newStateModify)
             return newStateModify
         case DELETETODO:
             const { todoId: indexTodo } = action.payload
 
-            return state.filter((todo) => todo.id !== indexTodo)
+            let newState3 = state.filter((todo) => todo.id !== indexTodo)
+
+            writeInLocalStorage(newState3)
+            return newState3
         default:
             return state
     }
